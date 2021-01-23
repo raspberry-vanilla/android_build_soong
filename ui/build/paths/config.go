a012aab664434e5b7d3e0948724af71236f51d23
Konsta <konsta09@gmail.com>
2021-01-23 20:53:21 +0200

    rpi4: whitelist tools for custom mkbootimg
    Change-Id: Ibae0adbf4ab93877db4dcd9a0124c1cd28c251fe

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -99,6 +99,8 @@ var Configuration = map[string]PathConfig{
 	"java":    Allowed,
 	"javap":   Allowed,
 	"lsof":    Allowed,
+	"mcopy":   Allowed,
+	"mkfs.fat":Allowed,
 	"openssl": Allowed,
 	"pstree":  Allowed,
 	"rsync":   Allowed,
